1211a2eb623843a2882f1bc9cf211e8ab7628519
Santiago Giner <santiagoginer@college.harvard.edu>
2023-12-02 17:38:27 -0500

    Add code to test learned policies and add comments

--- BC/main.py
+++ BC/main.py
@@ -11,6 +11,7 @@ import os
 ENV_NAME = "FlappyBird-v0"
 
 
+# Arguments
 def get_args():
     parser = argparse.ArgumentParser(description="Behavioral cloning")
     # Data directory
@@ -21,35 +22,41 @@ def get_args():
     parser.add_argument("--batch_size", type=int, default=64, help="Batch size")
     parser.add_argument("--num_dataset_samples", type=int, default=10000, help="Number of samples to start dataset off with")
     # Output directory
-    parser.add_argument("--policy_save_dir", default="./learned_policies", help="Directory to save learned policies")
+    parser.add_argument("--policy_save_dir", type=str, default="./learned_policies",
+        help="Directory to save learned policies")
+    # Number of tests to run when calling test.py
+    parser.add_argument("--n_tests", type=int, default=10, help="Number of tests to run")
     return parser.parse_args()
 
 
+# Learning through behavioral cloning (BC)
 def experiment(args):
-    # expert dataset loading
+    # Get expert data
     save_path = os.path.join(args.data_dir, f"{ENV_NAME}_dataset.pt")
     expert_dataset = torch.load(save_path)
     # Create environment
     env = gymnasium.make(ENV_NAME)
     state_dim = env.observation_space.shape[0]
     action_dim = env.action_space.n
-    
-    # Policy initialization
+    # Make a BC object and load training data into batches
     learner = BC(state_dim, action_dim, args)
     epoch_losses = []
     dataloader = get_dataloader(expert_dataset, args)
+    # Learn through gradient descent
     for _ in tqdm(range(1, args.epochs + 1)):
         loss = 0.0
         for batch in dataloader:
             loss += learner.learn(batch[0], batch[1])
         epoch_losses.append(loss / len(dataloader))
-    # Saving policy
+    # Save the learned policy
     outdir = args.policy_save_dir
     if not os.path.exists(args.policy_save_dir):
         os.makedirs(outdir)
     policy_save_path = os.path.join(outdir, f"{ENV_NAME}.pt")
     learner.save(policy_save_path)
 
+
+# Main
 if __name__ == "__main__":
     args = get_args()
     experiment(args)
---
+++ BC/test.py
@@ -0,0 +1,51 @@
+import flappy_bird_gymnasium
+import gymnasium
+from learner import *
+from main import get_args
+
+
+ENV_NAME = "FlappyBird-v0"
+
+
+# Test the learned policy
+def test(args):
+    # Make the environment
+    env = gymnasium.make(ENV_NAME, render_mode="human")
+    state_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.n
+    # Make a BC class and give it the learned policy
+    model = BC(state_dim, action_dim, args)
+    policy_save_path = os.path.join(args.policy_save_dir, f"{ENV_NAME}.pt")
+    model.load(policy_save_path)
+    # Arrays to store episode lengths and rewards
+    lengths = []
+    rewards = []
+    # Run the tests
+    for _ in range(args.n_tests):
+        # Initialize tracking of state and episode information
+        done = False
+        current_state, _ = env.reset()
+        length = 0
+        reward = 0
+        # Repeat until episode terminates
+        while not done:
+            # Render the game and get action from learned policy
+            env.render()
+            qs = model.get_logits(torch.from_numpy(current_state).float()) 
+            action = qs.argmax().numpy()
+            # Carry out the policy's action and keep track of length and reward
+            obs, r, done, _, info = env.step(action)
+            current_state = obs
+            length += 1
+            reward += r
+        # Save test length and reward
+        lengths.append(length)
+        rewards.append(reward)
+    print(f"Average episode length: {np.mean(lengths)}")
+    print(f"Average reward incurred: {np.mean(rewards)}")
+
+
+# Main
+if __name__ == '__main__':
+    args = get_args()
+    test(args)
