898b6ece2baa041162ae368fb404b54c65a67b7c
Ian <ianmmooreco@gmail.com>
2023-12-13 19:05:32 -0500

    small changes

--- ppo/ppo_test.py
+++ ppo/ppo_test.py
@@ -4,11 +4,14 @@ import flappy_bird_gymnasium
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 
-vec_env = make_vec_env("FlappyBird-v0", n_envs=4)
+vec_env = make_vec_env("FlappyBird-v0", n_envs=1)
 model = PPO.load("ppo_flappybird")
 
 obs = vec_env.reset()
+cumulative_reward = 0
 while True:
     action, _states = model.predict(obs)
     obs, rewards, dones, info = vec_env.step(action)
     vec_env.render("human")
+    cumulative_reward += rewards
+    print(cumulative_reward)
